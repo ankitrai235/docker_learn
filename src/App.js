80dbc4a44dab732bd3b3e1b95cdd01b753405fcf
Radha Chawla <radhank@Radhas-MacBook-Air.local>
2020-12-28 23:32:24 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ankit Rai made the chnage in the page!!
         </p>
         <a
           className="App-link"
